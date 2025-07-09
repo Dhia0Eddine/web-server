3255444201d049d33e7012ffc212ae30220683f1
Dhia0Guerfi <dhiaeddine.guerfi@ensia.edu.dz>
2025-07-09 11:44:22 +0100

    Add initial implementation of SimpleSocket and hdelibc headers

---
+++ Networking/SimpleSocket.hpp
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_SOCKET_HPP
+#define SIMPLE_SOCKET_HPP
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+namespace SimpleSocket {
+    private:
+
+    struct sockaddr
+
+}
+
+#endif
